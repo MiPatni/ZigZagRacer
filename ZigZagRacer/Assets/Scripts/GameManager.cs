086fbd427316934b3e61f9d01c19f23a2fa79516
DigiliateGames <DigiliateGames@gmail.com>
2021-08-09 13:31:26 +0500

    Basic Gameplay

---
+++ ZigZagRacer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public bool gameStarted;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameStarted)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                GameStart();
+            }
+        }
+    }
+
+    public void GameStart()
+    {
+        gameStarted = true;
+    }
+
+    public void GameOver()
+    {
+        gameStarted = false;
+    }
+}
